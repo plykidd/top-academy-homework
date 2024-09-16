ec4daebec9ba923d0abfca6a291ab45e5b56588d
plykidd <89293204471@yandex.ru>
2024-09-16 16:30:04 +0700

    17.09.2024 ДЗ

---
+++ 17.09.2024/17.09.2024 ДЗ.py
@@ -0,0 +1,64 @@
+# # Задание 1
+# # Реализуйте класс «Автомобиль». Необходимо хранить
+# # в полях класса: название модели, год выпуска, производителя, объем двигателя, цвет машины, цену. Реализуйте
+# # методы класса для ввода данных, вывода данных, реализуйте доступ к отдельным полям через методы класса.
+#
+#
+# class Car:
+#     def __init__(self, model, year, producer, engine_capacity, color_car, price):
+#         self.model = model
+#         self.year = year
+#         self.producer = producer
+#         self.engine_capacity = engine_capacity
+#         self.color_car = color_car
+#         self.price = price
+#
+#     def __str__(self):
+#         return f''' Автомобиль:
+#         Model: {self.model}
+#         Year: {self.year}
+#         Producer: {self.producer}
+#         EngineCapacity: {self.engine_capacity}
+#         ColorCar: {self.color_car}'''
+#
+#
+# car = Car('Mercedes', '2009', 'Germany', '3.0', 'black', '200000$')
+# print(car)
+
+# Задание 2
+# Реализуйте класс «Книга». Необходимо хранить в
+# полях класса: название книги, год выпуска, издателя,
+# жанр, автора, цену. Реализуйте методы класса для ввода
+# данных, вывода данных, реализуйте доступ к отдельным
+# полям через методы
+#
+# class Book:
+#     def __init__(self, name, year, producer, genre, author, price):
+#         self.name = name
+#         self.year = year
+#         self.producer = producer
+#         self.genre = genre
+#         self.author = author
+#         self.price = price
+#
+#     def __str__(self):
+#         return f''' Книга:
+#         Name: {self.name}
+#         Year: {self.year}
+#         Producer: {self.producer}
+#         Genre: {self.genre}
+#         Author: {self.author}
+#         Price: {self.price}'''
+#
+#
+# book = Book('War and World', '1805', 'L.N.Tolstoy', 'Roman', 'L.N.Tolstoy', '200$')
+# print(book)
+
+# Задание 3
+# Реализуйте класс «Стадион». Необходимо хранить в
+# полях класса: название стадиона, дату открытия, страну,
+# город, вместимость. Реализуйте методы класса для ввода
+# данных, вывода данных, реализуйте доступ к отдельным
+# полям через методы класса
+
+# Все тоже самое, просто поменяются название классов и переменных.
